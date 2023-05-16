f9dc46d48bae5edbd7fad1f4eadb45b7a00d2392
Orplane <131639075+Orplane@users.noreply.github.com>
2023-05-16 10:38:51 +0800

    Add files via upload

---
+++ ConsoleApp3/AppConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public void MyMethod()
+        {
+            string environment = GetEnvironment();
+            string key = "Key1";
+            string value = GetConfigValue(key, environment);
+
+            Console.WriteLine($"{key} Value: {value}");
+        }
+
+        private string GetEnvironment()
+        {
+#if DEBUG
+            return "Debug";
+#else
+                return "Release";
+#endif
+        }
+
+        private string GetConfigValue(string key, string environment)
+        {
+            NameValueCollection settings;
+
+            if (environment == "Debug")
+            {
+                settings = ConfigurationManager.GetSection("AppDebugSettings") as NameValueCollection;
+            }
+            else
+            {
+                settings = ConfigurationManager.GetSection("AppReleaseSettings") as NameValueCollection;
+            }
+
+            return settings?[key];
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass myObject = new MyClass();
+            myObject.MyMethod();
+        }
+    }
+}
